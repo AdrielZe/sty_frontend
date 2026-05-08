4133114b35aa53e98888054aa569059beb7cfdcf
adrielze <oadriel14@gmail.com>
2026-05-08 11:36:04 -0300

    release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.training_tracker"
+        applicationId = "com.adrdev.sty"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
