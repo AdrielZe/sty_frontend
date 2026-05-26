0c373a1a2f3a13a78c7c34c533448c23f70dce59
adrielze <oadriel14@gmail.com>
2026-05-26 17:46:08 -0300

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.adrdev.sty"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
